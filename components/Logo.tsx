5723201aaf1ae15c5a0c5576271cd330cc2f0734
AntonHansson21 <karl.anton.hansson@hotmail.com>
2026-05-24 17:45:29 +0200

    Fix logo path for GitHub Pages using NEXT_PUBLIC_BASE_PATH env var

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 export default function Logo({ size = 40 }: { size?: number }) {
   return (
-    <Image
-      src="/logo.svg"
+    <img
+      src={`${basePath}/logo.svg`}
       width={size}
       height={size}
       alt="Regmyr & Jansson logotyp"
